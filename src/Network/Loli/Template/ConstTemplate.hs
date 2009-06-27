560ba7f17599ba9921e248f7cef9013b8b5a0b58
jinjing <nfjinjing@gmail.com>
2009-06-28 01:21:27 +0800

    clean up const_template

--- src/Network/Loli/Template/ConstTemplate.hs
+++ src/Network/Loli/Template/ConstTemplate.hs
@@ -1,7 +1,5 @@
 module Network.Loli.Template.ConstTemplate where
   
-import Network.Loli.Config
-import Network.Loli.Template
 import Network.Loli.Type
 
 data ConstTemplate = ConstTemplate String
@@ -9,4 +7,5 @@ data ConstTemplate = ConstTemplate String
 instance Template ConstTemplate where
   interpolate (ConstTemplate x) _ = return x
 
+const_template :: String -> AppUnitT ConstTemplate
 const_template x = return $ ConstTemplate x
