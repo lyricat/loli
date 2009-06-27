e2cbddd6b861ee5dc80c1dc8e6f053b94e726e21
jinjing <nfjinjing@gmail.com>
2009-06-28 05:13:44 +0800

    simplify text_template

--- src/Network/Loli/Template/TextTemplate.hs
+++ src/Network/Loli/Template/TextTemplate.hs
@@ -6,16 +6,13 @@ import MPS
 import Network.Loli.Type
 import Prelude hiding ((.), (>), (^), (/))
 import Text.Template hiding (Context, Template, template)
-import qualified Data.ByteString.Lazy.Char8 as B
-import qualified Data.Map as Map
 import qualified Text.Template as T
-
-create_context :: [(String, String)] -> Map.Map B.ByteString B.ByteString
-create_context = map (fromString *** fromString) > to_h
 
 render_TextTemplate :: String -> Context -> IO String
 render_TextTemplate x c = 
   readTemplate (x.u2b) ^ flip T.render (create_context c) ^ toString
+  where
+    create_context = map (fromString *** fromString) > to_h
 
 data TextTemplate = TextTemplate String
 
